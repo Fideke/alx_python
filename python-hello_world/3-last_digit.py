b3a38cbf6c0fa97829f0faaff7ba74daf9d67534
Fideke <nadabafidel@gmail.com>
2023-07-18 10:30:42 +0300

    hello world

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,13 @@
+
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+if last_digit > 5:
+    answer = f"The string Last digit of {number} is {last_digit} and is greater than 5"
+elif last_digit == 0:
+     answer = f"The string Last digit of {number} is {last_digit} and is greater than 0"
+else:
+      answer = f"The string Last digit of {number} is {last_digit} and is less than not 0"
+
+print(answer)
