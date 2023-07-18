0a9fbbb619b22508ac1c0dc12412009f9940ba83
Fideke <nadabafidel@gmail.com>
2023-07-18 17:00:26 +0300

    hello world

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,7 +7,7 @@ if last_digit > 5:
     answer = f"The string Last digit of {number} is {last_digit} and is greater than 5"
 elif last_digit == 0:
      answer = f"The string Last digit of {number} is {last_digit} and is greater than 0"
-else:
+elif last_digit < 6 and last_digit != 0:
       answer = f"The string Last digit of {number} is {last_digit} and is less than not 0"
 
 print(answer)
