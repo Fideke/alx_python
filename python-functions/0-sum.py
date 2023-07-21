11dd0c3def64af1abc8b1a0ebcd4d5f471d88b45
Fideke <nadabafidel@gmail.com>
2023-07-21 23:15:54 +0300

    hello world

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -3,9 +3,9 @@ def add(a, b):
    sum = a + b
    return sum
     
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+print(add(1, 2), end="\n")
+print(add(98, 0), end="\n")
+print(add(100, -2), end="\n")
 
 
 
