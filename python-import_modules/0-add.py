4dad7e2d77ece21f2ee1b414c03ed64e4a42adec
Fideke <nadabafidel@gmail.com>
2023-07-26 23:18:54 +0300

    modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,6 @@
-import main
-from main import add
-print(add(1, 2))
-
-# a and be must be defined in 2 different lines a=1, b=2
-def add_0(a, b):
-    return a + b
-
-a = 1
-b = 2
-
-sum = add_0(a, b)
-
+from add_0 import add
+if __name__ == "__main__":
+ a = 1
+ b = 2
+sum = add(a, b)
 print(f"{a} + {b} = {sum}", end="\n")
