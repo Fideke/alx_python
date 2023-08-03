5cd8bf7e29381f188137bbaa7f1987bf9a8ffb20
Fideke <nadabafidel@gmail.com>
2023-08-03 10:43:04 +0300

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -27,6 +27,16 @@ my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
 
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+
 
 
         
