e1bda68864ce40d22708560c252f8f97c3b5de41
Fideke <nadabafidel@gmail.com>
2023-08-03 12:55:56 +0300

    python classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+"""
+square that definews a square by based on 0-square.py
+"""
+class Square:
+    
+  """
+   defining the class
+  """
+
+  def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
