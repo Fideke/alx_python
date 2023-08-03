91203f1d7c34a7474f661b5d3c9069b3758b4f31
Fideke <nadabafidel@gmail.com>
2023-08-03 23:18:14 +0300

    inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -37,6 +37,4 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
-    def area(self):
-        return self.__width * self.__height
+   
---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,46 @@
+"""
+an empty class
+"""
+class BaseGeometry:
+    """
+    empy class
+    """
+    pass
+
+    """
+    a function that defines area and raise exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+        """
+        a function that defines name , value
+        """
+    def integer_validator(self, name, value):
+        if value is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+
+"""
+a class rectangle that inherits from BaseGeometry
+"""
+class Rectangle(BaseGeometry):
+    """
+    inherited class
+    """
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__Width} / {self.__height}"
+      
