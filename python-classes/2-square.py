cfc2dcdb8762f1832093f5f4d41e33dd3850df63
Fideke <nadabafidel@gmail.com>
2023-08-03 13:47:38 +0300

    class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,21 +1,19 @@
-"""
-class square
-"""
+"""module with a class that defines a square"""
 class Square:
-    """
-    size must be an integer
-    define an area of a square
-    """
+    """defining square object
+    args:
+    size(param
+    ) is assumed to be an int"""
     def __init__(self, size=0):
-        self.__size = size
-        while size(type) is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            break
-        else:
-            raise TypeError("size must be an integer")
-    """
-    public instance method
-    """
+            self.__size = size 
+            """private attribute"""
+            while type(size) is int:
+                  if size < 0:
+                        raise ValueError("size must be >= 0")
+                  break
+            else:           
+                raise TypeError("size must be an integer")
+    """public instance method takes parameter self and can be accessed both inside and outside class"""
     def area(self):
-        return self.__size ** 2
+          """area"""
+          return self.__size ** 2
