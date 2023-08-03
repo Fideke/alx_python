511627fa3a7e78b903827fbfacb541691e7c6ee8
Fideke <nadabafidel@gmail.com>
2023-08-03 23:44:25 +0300

    inherit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -31,10 +31,13 @@ class Rectangle(BaseGeometry):
     inherited class
     """
     def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """
+        define a function
+        """
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = 0
+        self.__height = 0
    
