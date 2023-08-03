95488ec09b5208a51a12b82ba7ba7b976faa0eee
Fideke <nadabafidel@gmail.com>
2023-08-03 13:39:59 +0300

    classes

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+"""
+class square
+"""
+class Square:
+    """
+    size must be an integer
+    define an area of a square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        while size(type) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            break
+        else:
+            raise TypeError("size must be an integer")
+        
+        def area(self):
+            return self.__size ** 2
