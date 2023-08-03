d07d0a829593d721357a42b26987ddf1e0ea7cbd
Fideke <nadabafidel@gmail.com>
2023-08-03 11:21:12 +0300

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,34 +10,3 @@ class Square:
     """
     def __init__(self, size):
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
-    
-    def perimeter(self):
-        return 4 * self.__size
-    
-    def get_size(self):
-        return self.__size
-    
-    def set_size(self, size):
-        self.area__size = size
-    
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
-
-
-        
-  
