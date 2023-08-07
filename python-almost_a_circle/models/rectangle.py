5e259ff113e137923c65a748a83e643289df7087
Fideke <nadabafidel@gmail.com>
2023-08-07 23:30:42 +0300

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -29,74 +29,74 @@ class Rectangle(Base):
     @property
     def width(self):
          
-         """protect attribute of our class"""
+        """protect attribute of our class"""
 
-         return self.__width
+        return self.__width
         
     @property
     def height(self):
 
-            """protect attribute of our class"""
+        """protect attribute of our class"""
 
-            return self.__height
+        return self.__height
         
     @property
     def x(self):
          
-         """protect attribute of our class"""
+     """protect attribute of our class"""
 
-         return self.__x
+     return self.__x
         
     @property
     def y(self):
          
-         """protect attribute of our class"""
+     """protect attribute of our class"""
 
-         return self.__y
+     return self.__y
         
         
     @width.setter
     def width(self, value):    
-             """validate what adeveloper is trying to assign a variable"""
+        """validate what adeveloper is trying to assign a variable"""
 
-             if type(value) is not int:
-                raise TypeError("width must be an integer")
-             if value <= 0:
-                raise ValueError("width must be > 0")
-             self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
              
             
     @height.setter
     def height(self, value):
-            """validate what adeveloper is trying to assign a variable"""
+        """validate what adeveloper is trying to assign a variable"""
 
-            if type(value) is not int:
-               raise TypeError("height must be an integer")
-            if value <= 0:
-               raise ValueError("height must be > 0")
-            self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
     
 
     @x.setter
     def x(self, value):
-            """validate what adeveloper is trying to assign a variable"""
+        """validate what adeveloper is trying to assign a variable"""
 
-            if type(value) is not int:
-               raise TypeError("x must be an integer")
-            if value < 0:
-               raise ValueError("x must be >= 0")
-            self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
            
 
     @y.setter
     def y(self, value):
-            """validate what adeveloper is trying to assign a variable"""
+        """validate what adeveloper is trying to assign a variable"""
 
-            if type(value) is not int:
-               raise TypeError("y must be an integer")
-            if value < 0:
-               raise ValueError("y must be >= 0")
-            self.__y = value
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
             
 
 
