7e7d9a4121918f0797a64a842a3c97e254ff8dcc
Fideke <nadabafidel@gmail.com>
2023-08-07 15:38:25 +0300

    MODELS

---
+++ python-almost_a_circle/models/models/base.py
@@ -0,0 +1,18 @@
+"""
+create a file named models with an empty fil __init__.py,
+inside with this file, the folder will become python package
+"""
+class Base:
+   """
+   create a file named models/base.py
+   class base
+   class constructor: def __init__(self, id=None):
+   """
+   __nb_objects = 0
+
+   def __init__(self, id=None):
+      if id is not None:
+         self.id = id
+      else:
+          Base.__nb_objects =+ 1
+          self.id = Base
