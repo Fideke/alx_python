20b38617d759f98d6bf19cd50c6fc49d85e6115e
Fideke <nadabafidel@gmail.com>
2023-10-20 08:38:30 +0300

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """
-a python scripts that fetches https://alu-intranet.hbtn.io/status
+Module to fetch and display the status of a website.
 """
+
 import requests
+
 def fetch_hbtn_status():
     """
-     you must use the package requests
-     allowed to import requests only
+    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
     """
-
     url = 'https://alu-intranet.hbtn.io/status'
     response = requests.get(url)
-    context_type = type(response.text)
+    content_type = type(response.text)
     content = response.text
 
-    print("Body response")
-    print(f"\t-type: {context_type}")
-    print(f"\t-content: {content}")
+    print("Body response:")
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {content}")
 
 if __name__ == "__main__":
     fetch_hbtn_status()
